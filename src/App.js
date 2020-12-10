1fb1d712b8a6854f2d7ac03e02894a7b58b9c690
Mark Lopresto <marklopresto@Marks-MacBook-Air.local>
2020-12-09 22:32:04 -0500

    remove all for MoviesList

--- src/App.js
+++ src/App.js
@@ -1,32 +1,13 @@
 import React from 'react'
-import PropTypes from 'prop-types' // updated way since React v15.5
-import { Counter } from './components/Counter'
-import { Accordian } from './components/Accordian';
 import './App.css';
+import { MoviesList } from './components/movies/MoviesList';
 
 function App() {
   return (
     <div className="App">
-      <HelloWorld />
-      <Counter />
-      <Accordian />
+      <MoviesList />
     </div>
   );
-}
-
-function HelloWorld({ greeting = "Hi", name = "Mark", day = "Saturday" }) { // de-structure props {}
-  return (
-  <div>
-    <h1>{greeting} {name}</h1>
-    <h3>Today is {day}</h3>
-  </div>
-  )
-}
-
-HelloWorld.propTypes = {
-  name: PropTypes.string,
-  greeting: PropTypes.string,
-  day: PropTypes.string
 }
 
 export default App;
